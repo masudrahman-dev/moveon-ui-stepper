54342b073539eb705f735a5a9c63d88434801fc1
masudrahman-dev <masudrahman.dev@gmail.com>
2024-02-29 18:35:01 +0600

    update sidebar

--- src/components/side-navigation-menu/index.tsx
+++ src/components/side-navigation-menu/index.tsx
@@ -1,8 +1,9 @@
+import { ArrowRightMini, TriangleDownMini } from "@medusajs/icons";
 import React, { useEffect, useState } from "react";
 
 const GrandChildMenu = ({ grandChildMenu }) => {
   return (
-    <ul className="group-hover/child:flex  flex-col gap-3   divide-y   p-3 w-48 absolute border hidden -right-48 top-0 z-50">
+    <ul className="group-hover/child:flex  flex-col gap-3      p-3 w-48 absolute border hidden -right-[185px] top-0 z-50">
       {grandChildMenu?.map((subItem) => {
         console.log("subItem :>> ", subItem);
         return (
@@ -22,7 +23,7 @@ const GrandChildMenu = ({ grandChildMenu }) => {
 
 const ChildMenu = ({ clildMenu }) => {
   return (
-    <ul className="group-hover/parent:flex  flex-col gap-3   divide-y  p-3 w-48 absolute border hidden -right-48 top-0 z-50">
+    <ul className="group-hover/parent:flex  flex-col gap-3  hidden    p-3 w-52 absolute border  -right-[210px] top-0 z-50">
       {clildMenu?.map((subItem) => {
         return (
           <li
@@ -31,7 +32,9 @@ const ChildMenu = ({ clildMenu }) => {
           >
             <div className="flex justify-between">
               <span>{subItem?.title}</span>
-              <span className="group-hover/child:block hidden">{`>`}</span>
+              <span className="group-hover/child:block hidden">
+                <ArrowRightMini />
+              </span>
             </div>
             <GrandChildMenu grandChildMenu={clildMenu} />
           </li>
@@ -59,16 +62,18 @@ const SideNavigationMenu = () => {
     <div>
       <div className="mx-auto   mt-10 max-w-screen-lg">
         <div>
-          <ul className="flex flex-col gap-3   divide-y">
+          <ul className="flex flex-col gap-3   ">
             {data?.map((item) => {
               return (
                 <li
                   key={item?.id}
-                  className="odd:bg-white w-1/2 group/parent relative hover:text-orange-500"
+                  className="odd:bg-white w-1/2 group/parent border-b relative hover:text-orange-500"
                 >
                   <div className="flex justify-between">
                     <span>{item?.title}</span>
-                    <span className="group-hover/parent:block hidden">{`>`}</span>
+                    <span className="group-hover/parent:block hidden">
+                      <ArrowRightMini />
+                    </span>
                   </div>
                   <ChildMenu clildMenu={item?.childCategories} />
                 </li>
